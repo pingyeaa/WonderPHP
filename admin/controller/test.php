01222846dd90185f774dd6042d053c4cbdf89cb0
EnochZg <15989529532@163.com>
2015-04-09 18:58:55 +0800

    smarty

---
+++ admin/controller/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhangyinuo
+ * Date: 15/4/9
+ * Time: 下午5:47
+ */ 
--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
+/**
+ * 入口文件
+ * @author Enoch
+ * @date 2015-04-09
+ */
 
-echo 123;
+# 1、定义根目录
+define('ROOT_PATH', __DIR__);
+
+# 2、引入自动加载类
+require_once ROOT_PATH . '/' . 'framework/libs/LClassLoader.php';
+
+# 3、实例化WonderPHP并运行
+\framework\WonderPHP('home')::run();
